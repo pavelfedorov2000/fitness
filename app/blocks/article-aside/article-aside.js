5d6ed01abe0660c8254247640fa868d408a7cc7d
Pavel Fedorov <p.fedorov@imedia.by>
2022-08-12 17:27:28 +0300

    12.08 article-page

--- app/blocks/article-aside/article-aside.js
+++ app/blocks/article-aside/article-aside.js
@@ -1,7 +1,29 @@
-app.yourScriptName = {
-  name: 'yourScriptName',
-  description: 'your script description',
-  init() {
-    // your code
-  },
+app.articleAside = {
+    name: 'articleAside',
+    description: 'your script description',
+    init() {
+        function fixSidebar() {
+            if (window.innerWidth > 1024) {
+                new StickySidebar('.article-page__sidebar', {
+                    topSpacing: 0,
+                    bottomSpacing: 32,
+                    containerSelector: '.article-page__inner',
+                    innerWrapperSelector: '.article-page__sidebar-inner'
+                });
+
+                new StickySidebar('.article-page__social', {
+                    topSpacing: 95,
+                    bottomSpacing: 32,
+                    containerSelector: '.article-page__container',
+                    innerWrapperSelector: '.article-page__social .social'
+                });
+            }
+        }
+
+        fixSidebar();
+
+        window.addEventListener('resize', () => {
+            fixSidebar();
+        });
+    },
 };
