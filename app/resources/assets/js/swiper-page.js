0f5979703809900516cb4bd46be2d26f0ef76a36
Pavel Fedorov <p.fedorov@imedia.by>
2022-08-17 19:16:25 +0300

    coach page start

---
+++ app/resources/assets/js/swiper-page.js
@@ -0,0 +1,56 @@
+$(function () {
+    if (document.querySelectorAll('.page-slider').length) {
+        document.querySelector('.footer').classList.add('swiper-slide');
+    }
+
+    function initSlider() {
+        if (window.innerWidth > 1024 && !document.querySelector(".page-slider").classList.contains("swiper-initialized")) {
+            const pageSlider = new Swiper(".page-slider", {
+                direction: "vertical",
+                spaceBetween: 0,
+                slidesPerView: "auto",
+                speed: 800,
+                keyboard: {
+                    enabled: true,
+                    onlyInViewport: true,
+                    pageUpDown: true
+                },
+                mousewheel: {
+                    sensitivity: 1,
+                },
+                watchOverflow: true,
+                init: false,
+                allowTouchMove: false,
+                parallax: true,
+                on: {
+                    init: function (swiper) {
+                        document.querySelector('.swiper-slide-active').classList.remove('active');	
+                    },
+                    slideChange: function (swiper) {
+                        if (!document.querySelector('.swiper-slide-active').classList.contains('active')) {
+                            document.querySelector('.swiper-slide-active').classList.add('active');
+                        } else {
+                            document.querySelector('.swiper-slide-active').classList.remove('active');
+                        }
+                    },
+                    slideChangeTransitionEnd: function (swiper) {
+                        if (!document.querySelector('.swiper-slide-active').classList.contains('active')) {
+                            document.querySelector('.swiper-slide-active').classList.add('active');
+                        } else {
+                            document.querySelector('.swiper-slide-active').classList.remove('active');
+                        }
+                    },
+                }
+            });
+        
+            pageSlider.init();
+        } else if (window.innerWidth < 1024 && document.querySelector(".page-slider").classList.contains("swiper-initialized")) {
+            pageSlider.destroy(true, true);
+        }
+    }
+    initSlider();
+
+    window.addEventListener('resize', function () {
+        initSlider();
+    });
+});
